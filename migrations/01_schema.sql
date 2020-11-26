2ad3640a7d6b7d224e65de2448fad42d446ed557
Marcio Valle De Oliveira e Cruz <mvocruz@hotmail.com>
2020-11-26 00:46:39 +0000

    adjust data fields to receive more data

--- migrations/01_schema.sql
+++ migrations/01_schema.sql
@@ -16,37 +16,35 @@ CREATE TABLE properties (
 
   title VARCHAR(255) NOT NULL,
   description TEXT,
-  thumbnail_url VARCHAR(255) NOT NULL,
-  cover_photo VARCHAR(255) NOT NULL,
-  cost_per_night INTEGER NOT NULL DEFAULT 0,
-  parking_spaces INTEGER DEFAULT 0,
-  number_of_bathrooms INTEGER DEFAULT 0,
-  numbert_of_bedrooms INTEGER DEFAULT 0,
+  thumbnail_photo_url VARCHAR(255) NOT NULL,
+  cover_photo_url VARCHAR(255) NOT NULL,
+  cost_per_night INTEGER  NOT NULL DEFAULT 0,
+  parking_spaces INTEGER  NOT NULL DEFAULT 0,
+  number_of_bathrooms INTEGER  NOT NULL DEFAULT 0,
+  number_of_bedrooms INTEGER  NOT NULL DEFAULT 0,
 
-  country VARCHAR (255) NOT NULL,
-  province VARCHAR (255) NOT NULL,
-  city VARCHAR (255) NOT NULL,
-  street VARCHAR (255)NOT NULL,
-  post_code VARCHAR (255)NOT NULL,
+  country VARCHAR(255) NOT NULL,
+  street VARCHAR(255) NOT NULL,
+  city VARCHAR(255) NOT NULL,
+  province VARCHAR(255) NOT NULL,
+  post_code VARCHAR(255) NOT NULL,
 
-  is_active BOOLEAN NOT NULL DEFAULT TRUE
+  active BOOLEAN NOT NULL DEFAULT TRUE
 );
 
 CREATE TABLE reservations (
   id SERIAL PRIMARY KEY NOT NULL,
-  guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-  property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE,
-
   start_date DATE NOT NULL,
-  end_date DATE NOT NULL
+  end_date DATE NOT NULL,
+  property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE,
+  guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE
 );
 
 CREATE TABLE property_reviews (
   id SERIAL PRIMARY KEY NOT NULL,
-  reservation_id INTEGER REFERENCES reservations(id) ON DELETE CASCADE,
   guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
   property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE,
-
-  message TEXT,
-  rating SMALLINT NOT NULL DEFAULT 0
+  reservation_id INTEGER REFERENCES reservations(id) ON DELETE CASCADE,
+  rating SMALLINT NOT NULL DEFAULT 0,
+  message TEXT
 );
